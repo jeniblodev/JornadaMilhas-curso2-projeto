c0df15b464b0ced5f61f5f44b2b915b9912200ff
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2024-02-16 14:26:53 -0300

    video 3.1

--- JornadaMilhas.Test.Integracao/ContextoFixture.cs
+++ JornadaMilhas.Test.Integracao/ContextoFixture.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Testcontainers.MsSql;
 
 namespace JornadaMilhas.Test.Integracao;
 public class ContextoFixture
@@ -12,11 +13,14 @@ public class ContextoFixture
     public ContextoFixture()
     {
         var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
-        .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=JornadaMilhas;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False")
+        .UseSqlServer(_mssqlContainer.GetConnectionString())
         .Options;
 
          Context = new JornadaMilhasContext(options);
     }
 
     public JornadaMilhasContext Context { get; }
+    private readonly MsSqlContainer _mssqlContainer = new MsSqlBuilder()
+    .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+    .Build();
 }
