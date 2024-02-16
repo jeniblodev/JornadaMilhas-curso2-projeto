b8d0d9ca0ef70023601f19407a2ec9d3d98a6b3a
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2024-02-16 14:40:52 -0300

    video 3.2

--- JornadaMilhas.Test.Integracao/ContextoFixture.cs
+++ JornadaMilhas.Test.Integracao/ContextoFixture.cs
@@ -8,19 +8,26 @@ using System.Threading.Tasks;
 using Testcontainers.MsSql;
 
 namespace JornadaMilhas.Test.Integracao;
-public class ContextoFixture
+public class ContextoFixture: IAsyncLifetime
 {
-    public ContextoFixture()
+
+    public JornadaMilhasContext Context { get; private set; }
+    private readonly MsSqlContainer _mssqlContainer = new MsSqlBuilder()
+    .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+    .Build();
+
+    public async Task InitializeAsync()
     {
+        await _mssqlContainer.StartAsync();
         var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
         .UseSqlServer(_mssqlContainer.GetConnectionString())
         .Options;
 
-         Context = new JornadaMilhasContext(options);
+        Context = new JornadaMilhasContext(options);
     }
 
-    public JornadaMilhasContext Context { get; }
-    private readonly MsSqlContainer _mssqlContainer = new MsSqlBuilder()
-    .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
-    .Build();
+    public async Task DisposeAsync()
+    {
+        await _mssqlContainer.StopAsync();
+    }
 }
